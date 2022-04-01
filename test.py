d3ab02a22303ce202c64b96f6bfed04713c6c305
antti <antti@nano.localdomain>
2022-04-01 18:39:24 +0300

    save video procress

--- test.py
+++ test.py
@@ -1,32 +1,29 @@
+import sys
 import cv2
 
-video = cv2.VideoCapture(0)
+def read_cam():
+    video = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink")
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+    size = (frame_width, frame_height)
 
-if (video.isOpened() == False):
-    print("Error reading video file")
+    result = cv2.VideoWriter('test.avi',
+                        cv2.VideoWriter_fourcc(*'MJPG'),
+                        10, size)
 
-frame_width = int(video.get(3))
-frame_height = int(video.get(4))
-size = (frame_width, frame_height)
-
-result = cv2.VideoWriter('test.avi',
-                         cv2.VideoWriter_fourcc(*'MJPG'),
-                         10, size)
-
-while (True):
-    ret, frame = video.read()
-
-    if ret == True:
-
-        result.write(frame)
-
-        if cv2.waitKey(32) & 0xFF == ord('s'):
-            break
+    if video.isOpened():
+        cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
+        while True:
+            ret_val, frame = video.read()
+            result.write(frame)
     else:
-        break
+        print ("camera open failed")
 
-video.release()
-result.release()
+    video.release()
+    result.release()
+    cv2.destroyAllWindows()
+    print("The video was successfully saved")
 
-cv2.destroyAllWindows()
-print("The video was successfully saved")
+
+if __name__ == '__main__':
+    read_cam()
